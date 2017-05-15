4baef0edb46f8d8cb6821300802264565ba3acef
Dave W <dviper@gmail.com>
2017-05-15 15:54:44 -0600

    set up insertion sort files

---
+++ scripts/insertionSort.js
@@ -0,0 +1,5 @@
+function insertionSort (array) {
+  
+}
+
+export default insertionSort;
---
+++ tests/insertionSort-test.js
@@ -0,0 +1,21 @@
+import {expect, assert} from 'chai';
+import insertionSort from '../scripts/insertionSort.js'
+import genRanNum from '../scripts/genRanNum.js'
+
+describe('Insertion Sort', () => {
+  it('Should be a function', () => {
+    assert.isFunction(insertionSort);
+  })
+
+  it.skip('Should sort an unsorted array of numbers', () => {
+
+  })
+
+  it.skip('Should sort large arrays', () => {
+
+  })
+
+  it.skip('Should move min number to front', () => {
+
+  })
+})
