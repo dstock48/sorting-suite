e15564ace5e8064ce1a75695a848aa2af53951e7
Dave W <dviper@gmail.com>
2017-05-18 09:12:00 -0600

    add sort.js file with imports for all other sorting files

---
+++ sort.js
@@ -0,0 +1,6 @@
+import bubbleSort from './scripts/bubbleSort';
+import insertionSort from './scripts/insertionSort';
+import mergeSort from './scripts/mergeSort';
+import quickSort from './scripts/quickSort';
+
+export default { bubbleSort, insertionSort, mergeSort, quickSort }
